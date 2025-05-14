cd385c5fad4203314f214353b43374f019195317
Roman <sqrap@mail.ru>
2025-05-14 12:31:14 +0300

    # доработка Вот основные недочеты:
     В проекте есть .gitignore
    
     В коммиты не добавлены игнорируемые файлы
    
     Атрибуты экземпляра класса - приватные
    
     Метод подключения к API HH - приватный
    
     В классе используется __slots__ для экономии памяти
    
     У каждого экземпляра вакансии есть минимум 4 атрибута
    
     Все классы задокументированы
    
     Все методы классов задокументированы
    
     Все методы классов типизированы

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
+import requests
+from typing import List, Dict
 from src.api.hh_api import HeadHunterAPI
 from src.models.vacancy import Vacancy
 from src.storage.json_server import JSONserver
 
 
 def user_interaction():
+    """Основная функция взаимодействия с пользователем"""
     hh_api = HeadHunterAPI()
     json_server = JSONserver()
 
     search_query = input("ВВедите поисковый запрос: ")
     top_n = int(input("Введите количество вакансий для вывода в топ N: "))
+    try:
+        hh_vacancies: List[Dict] = hh_api.get_vacancies(search_query)
 
-    hh_vacancies = hh_api.get_vacancies(search_query)
+        vacancies_list: List[Dict] = Vacancy.cast_to_object_list(hh_vacancies)
 
-    vacancies_list = Vacancy.cast_to_object_list(hh_vacancies)
+        for vacancy in vacancies_list:
+            json_server.add_vacancy(vacancy)
 
-    for vacancy in vacancies_list:
-        json_server.add_vacancy(vacancy)
+        sorted_vacancies: List[Vacancy] = sorted(vacancies_list)[:top_n]
 
-    sorted_vacancies = sorted(vacancies_list)[:top_n]
-
-    for v in sorted_vacancies:
-        print(f"{v.title} - {v.salary} - {v.url}")
-
+        for v in sorted_vacancies:
+            print(f"{v.title} - {v.salary} - {v.url}")
+    except requests.RequestException as e:
+        print(f"Ошибка при получении данных от API: {e}")
+    except ValueError as e:
+        print(f"Неккоректный ввод числа: {e}")
 
 if __name__ == '__main__':
     user_interaction()
--- src/api/hh_api.py
+++ src/api/hh_api.py
@@ -1,11 +1,23 @@
 import requests
-from .abstract_api import AbstractAPI
+from typing import List, Dict
 
 
-class HeadHunterAPI(AbstractAPI):
+class HeadHunterAPI:
+    """Класс для взаимодействия с апи ххру"""
+
+    __slots__ = ()
+
+    def __init__(self):
+        pass
+
+    def connect(self) -> requests.Session:
+        session = requests.Session()
+        return session
+
     BASE_URL = "http://api.hh.ru/vacancies"
 
-    def get_vacancies(self, query: str):
+    def get_vacancies(self, query: str) -> List[Dict]:
+        session = self.__connect()
         response = requests.get(self.BASE_URL, params={"text": query})
         response.raise_for_status()
         return response.json().get('items', [])
